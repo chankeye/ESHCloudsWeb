41c660ddef1d14b21f5bcf644bf59305d6ba6403
chankeye <chankeye.chen@afusion.com>
2015-01-12 10:14:18 +0800

    update

--- MemberCRUD/DTO/DepartData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ESHCloudsWeb.DTO
-{
-    public class DepartData
-    {
-        public int DepartID { get; set; }
-        public string DepartName { get; set; }
-    }
-}
--- MemberCRUD/DTO/FactoryData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ESHCloudsWeb.DTO
-{
-    public class FactoryData
-    {
-        public int FactoryID { get; set; }
-        public string FactoryName { get; set; }
-    }
-}
